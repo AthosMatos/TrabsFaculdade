0c237d94699e54137dae2f15214f3fa647e7301f
Athos Matos <ladiesman217.as@gmail.com>
2022-04-22 18:03:25 -0300

    up

--- trabsCompiladores/my-app/src/Page/Editor/Editor.js
+++ trabsCompiladores/my-app/src/Page/Editor/Editor.js
@@ -7,18 +7,13 @@ import './Editor.css';
 const Editor = () => 
 {
     const [line,setLine] = useState()
-    const [CurrentToken,setCurrentToken] = useState()
+    const [CurrentToken,setCurrentToken] = useState('')
+    const [PrevTokens,setPrevTokens] = useState([])
     const [TokenID,setTokenID] = useState('instituicao')
-    const [blockText,setBlockText] = useState(false)
+    const [TokenIndex,setTokenIndex] = useState()
     const [warning,setWarning] = useState()
     const [lineIndex,setlineIndex] = useState(0)
-    const [Helpers,setHelpers] = useState(false)
     const editor = useMemo(() => withHistory(withReact(createEditor())),[])
-
-    useEffect(()=>
-    {
-        if(CurrentToken)console.log("CurrentToken",CurrentToken)
-    })
 
     const isNumber = (key) =>
     {
@@ -40,7 +35,6 @@ const Editor = () =>
             {   
                 if(CurrentLine[i] === ' ' && CurrentLine[i + 1] === ' ' && CurrentLine[CurrentLine.length - 1] !== ' ') 
                 {
-                    setBlockText(true)
                     setWarning("Mais de um espaco entre as palavras")
                     console.log("Espaco mais de uma vez, ERRO")
 
@@ -49,15 +43,12 @@ const Editor = () =>
             }
         }
         
-        setBlockText(false)
         setWarning() 
     } 
 
-    const CheckLine = (value) =>
+    const CheckTXT = (value) =>
     {
         let CurrentLine = value[checkLine(value)].children[0].text
-
-        setBlockText(false)
 
         setLine(CurrentLine)    
        
@@ -69,23 +60,38 @@ const Editor = () =>
 
         analyzSpaces(value)
     }
+    
+    const saveToken = () =>
+    {
+        console.log('PrevTokens',PrevTokens)
+                
+        setPrevTokens((prevstate) =>
+        {
+            let array = prevstate
+            array.push({token:CurrentToken,id:TokenID})
+            return array
+        })
+    }
 
     const CheckKeyPressed = (event) =>
     {
-        //console.log('key',key)
+        console.log("CurrentToken",CurrentToken) 
+
         let addtotoken = true
 
         if (event.key === 'Enter')
         {
             addtotoken = false
 
-            if(line!=='')
+            if(CurrentToken!=='')
             {
                 if(TokenID==='instituicao')
                 {
                     //ANALIZAR 
                     // console.log('Analisar token',Token)
-                    setCurrentToken()
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
                     event.preventDefault()
                     editor.insertBreak()
                     editor.insertText('MEMORANDO no. ')
@@ -94,49 +100,333 @@ const Editor = () =>
                 }
                 else if(TokenID==='numero do memorando')
                 {
-                    setCurrentToken()
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
                     setTokenID('local')
                     setlineIndex(lineIndex + 1)
                 }
-                else if(TokenID==='local')  event.preventDefault()   
-                else if(TokenID==='dia') event.preventDefault()
-                else if(TokenID==='mes') event.preventDefault()
-                else if(TokenID==='ano') event.preventDefault()
-                
+                else if(TokenID==='local')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText(', ')
+                    setTokenID('dia')
+                } 
+                else if(TokenID==='dia')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText(' de ')
+                    setTokenID('mes')
+                }
+                else if(TokenID==='mes')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText(' de ')
+                    setTokenID('ano')
+                }
+                else if(TokenID==='ano')
+                {
+                    if(CurrentToken.length===4)
+                    {
+                        saveToken()
+                        addtotoken = false
+                        setCurrentToken('')
+                        event.preventDefault()
+                        editor.insertText('.')
+                        editor.insertBreak()
+                        editor.insertBreak()
+                        editor.insertText('De: Departamento de ')
+                        
+                        setTokenID('depto proponente')
+                    }
+                    else event.preventDefault()
+                }
+                else if(TokenID==='depto proponente')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertBreak()
+                    editor.insertText('Para: Departamento de ')
+                    
+                    setTokenID('depto dest')
+                }
+                else if(TokenID==='depto dest')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    if(CurrentToken[CurrentToken.length - 1] === ' ')
+                    {
+                        editor.insertText('/ ')
+                    }
+                    else 
+                    {
+                        editor.insertText(' / ')
+                    }
+                    setTokenID('pronome tratamento')
+                } 
+                else if(TokenID==='pronome tratamento')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText('. ')
+                    setTokenID('cargo dest')
+                } 
+                else if(TokenID==='cargo dest') 
+                { 
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText(' ')
+                    setTokenID('nome dest')
+                }
+                else if(TokenID==='nome dest')
+                {    
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertBreak()
+                    editor.insertText('Assunto: ')
+                    setTokenID('assunto')
+                }
+                else if(TokenID==='assunto')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertBreak()
+                    editor.insertBreak()
+                    for(let i = 0; i < PrevTokens.length; i++)
+                    {
+                        if(PrevTokens[i].id === 'pronome tratamento')
+                        {
+                            editor.insertText(PrevTokens[i].token+'. ')
+                        }
+                        else if(PrevTokens[i].id === 'cargo dest')
+                        {
+                            editor.insertText(PrevTokens[i].token)
+                        }
+                    }
+                    editor.insertBreak()
+                    setTokenID('descricao')
+                }
+                else if(TokenID==='descricao')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertBreak()
+                    editor.insertBreak()
+                    editor.insertText(`Atenciosamente,`)
+                    editor.insertBreak()
+                    editor.insertBreak()
+                    setTokenID('nome proponente')
+                }
+                else if(TokenID==='nome proponente')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertBreak()
+                    setTokenID('cargo proponente')
+                }
+                else if(TokenID==='cargo proponente')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    if(CurrentToken[CurrentToken.length - 1] === ' ')
+                    {
+                        editor.insertText(`do Departamento `)
+                    }
+                    else 
+                    {
+                        editor.insertText(` do Departamento `)
+                    }
+
+                    for(let i = 0; i < PrevTokens.length; i++)
+                    {
+                        if(PrevTokens[i].id === 'depto proponente')
+                        {
+                            editor.insertText(PrevTokens[i].token)
+                        }
+                    }
+                }
+            }
+            else event.preventDefault()
+        }
+        else if (event.key === ' ')
+        {
+            if(CurrentToken!=='')
+            {
+                if(TokenID==='numero do memorando')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertBreak()
+                    setTokenID('local')
+                }
+                else if(TokenID==='local')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText(', ')
+                    setTokenID('dia')
+                } 
+                else if(TokenID==='dia')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText(' de ')
+                    setTokenID('mes')
+                }
+                else if(TokenID==='mes')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText(' de ')
+                    setTokenID('ano')
+                }
+                else if(TokenID==='ano')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText('.')
+                    editor.insertBreak()
+                    editor.insertBreak()
+                    editor.insertText('De: Departamento de ')
+                    
+                    setTokenID('depto proponente')
+                }
+                else if(TokenID==='pronome tratamento')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText('. ')
+                    setTokenID('cargo dest')
+                } 
+                else if(TokenID==='cargo dest')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText(' ')
+                    setTokenID('nome dest')
+                } 
+                else if(TokenID==='nome dest')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertBreak()
+                    editor.insertText('Assunto: ')
+                    setTokenID('assunto')
+                } 
+                else if(TokenID==='cargo proponente')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText(` do Departamento `)
+
+                    for(let i = 0; i < PrevTokens.length; i++)
+                    {
+                        if(PrevTokens[i].id === 'depto proponente')
+                        {
+                            editor.insertText(PrevTokens[i].token)
+                        }
+                    }
+                }
+            }
+            else 
+            {
+                event.preventDefault()  
+                addtotoken = false
             }
         }
         else if(event.key === ',')
         {
             if(TokenID==='local')
             {
+                saveToken()
                 addtotoken = false
-                setCurrentToken()
+                setCurrentToken('')
                 event.preventDefault()
                 editor.insertText(', ')
                 setTokenID('dia')
             } 
         }
-        else if (event.key === ' ')
+        else if(event.key === '.')
         {
-            if(TokenID==='dia')
+            if(TokenID==='pronome tratamento')
             {
+                saveToken()
                 addtotoken = false
-                setCurrentToken()
+                setCurrentToken('')
                 event.preventDefault()
-                editor.insertText(' de ')
-                setTokenID('mes')
-            }
+                editor.insertText('. ')
+                setTokenID('cargo dest')
+            } 
         }
-        else if(event.key === 'Backspace')
+        else if(event.key === '/')
         {
-            if(TokenID==='mes')
+            if(TokenID==='depto dest')
             {
-                setCurrentToken()
-                event.preventDefault()
-                editor.insertText(' de ')
-                setTokenID('ano')
-            }
-            
+                if(CurrentToken[CurrentToken.length - 1] === ' ')
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText('/ ')
+                    setTokenID('pronome tratamento')
+                }
+                else 
+                {
+                    saveToken()
+                    addtotoken = false
+                    setCurrentToken('')
+                    event.preventDefault()
+                    editor.insertText(' / ')
+                    setTokenID('pronome tratamento')
+                }
+                
+            } 
         }
         else
         {
@@ -151,6 +441,38 @@ const Editor = () =>
             }
             if(TokenID==='dia')
             {
+                if(CurrentToken.length >= 2)
+                {
+                    event.preventDefault()
+                    addtotoken = false
+                }
+                if(!isNumber(event.key)) 
+                {
+                    event.preventDefault()
+                    addtotoken = false
+                }
+               
+            }
+            if(TokenID==='mes')
+            {
+                if(isNumber(event.key)) 
+                {
+                    event.preventDefault()
+                    addtotoken = false
+                }
+               
+            }
+            if(TokenID==='ano')
+            {
+                if(CurrentToken.length >= 4)
+                {
+                    if(event.key !== 'Backspace')
+                    {
+                        event.preventDefault()
+                        addtotoken = false
+                    }
+                   
+                }
                 if(!isNumber(event.key)) 
                 {
                     event.preventDefault()
@@ -166,16 +488,31 @@ const Editor = () =>
             {
                 if(event.key === 'Backspace')
                 {
-                    if(prevstate) return prevstate.slice(0,prevstate.length - 1)
+                    if(prevstate.length > 0) return prevstate.slice(0,prevstate.length - 1)
                     else return prevstate
                 }
-                if(prevstate) return prevstate + event.key
-                else return event.key
+                if(CanAdd(event.key))
+                {
+                    if(prevstate.length > 0) return prevstate + event.key
+                    else return event.key
+                }
+                else return prevstate
                
             })
         }
 
         adjustLinebyArrows(event.key)
+    }
+
+    const CanAdd = (key) =>
+    {
+        if(key==='CapsLock')return false
+        else if(key==='ArrowUp')return false
+        else if(key==='ArrowDown')return false
+        else if(key==='ArrowLeft')return false
+        else if(key==='ArrowRight')return false
+        else if(key==='Tab')return false
+        return true
     }
 
     const adjustLinebyArrows = (key) =>
@@ -219,33 +556,25 @@ const Editor = () =>
             flexDirection:'row',
             marginLeft:'5%'
             }}>
-            <p style={{color:'white', color: 'rgb(140, 140, 140)',}}>Voce esta editando: </p>
+            <p style={{color: 'rgb(140, 140, 140)',}}>Voce esta editando: </p>
             <p style={{color:'rgb(0, 200, 0)', marginLeft:'0.4rem',}}>{TokenID}</p>
             <p style={{color:'rgb(200, 0, 0)', marginLeft:'0.4rem',}}>{warning}</p>
         
         </div>
         
         <div className='EditorContainer'>
-            <Slate editor={editor} value={initialText}  onChange={(value)=>
+            <Slate editor={editor} value={initialText} onChange={(value)=>
             { 
-                CheckLine(value)
+                CheckTXT(value)
             }}>
                 <Editable 
                 className='textContainer'
                 placeholder="Digite aqui..." 
                 onKeyDown={(event)=>
-                { 
-                    if(Helpers)
-                    {
-                        if(blockText) 
-                        {
-                            event.preventDefault()//only with helper on
-                            setBlockText(false)
-                        }
-                    }
+                {                     
                     CheckKeyPressed(event)
                 }}
-
+                
                 />
             </Slate>
         </div>    
